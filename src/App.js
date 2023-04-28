7822f1c6ff5604ee3247d39c48767a89f37566e0
Tommy Leong <tommymoleong@gmail.com>
2023-04-28 17:02:03 +0800

    Update task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import 'h8k-components';
 
@@ -8,15 +8,50 @@ const title = "Sorting Articles";
 
 function App({articles}) {
 
+    const [processedArticle, setProcessedArticle] = useState(articles)
+
+    useEffect(()=>{
+        sortBy();
+    },[]);
+
+    const sortBy = (type = 'mostRecent') => {
+        console.log('fired')
+        switch(type){
+            case 'upvote':
+                articles.sort((a,b)=>{
+                    if(a.upvotes < b.upvotes){
+                        return +1
+                    }
+        
+                    if(a.upvotes > b.upvotes){
+                        return -1
+                    }
+        
+                    return 0;
+                })        
+                break;
+            case 'mostRecent':
+                articles.sort((a,b)=>{
+                    return a.date - b.date
+                })        
+                break;
+            default:
+                // default as upvotes already    
+                break;
+        }
+        console.log(articles);
+        setProcessedArticle(articles)
+    }
+
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button data-testid="most-upvoted-link" className="small" onClick={()=>{sortBy('upvotes')}} >Most Upvoted</button>
+                <button data-testid="most-recent-link" className="small" onClick={()=>{sortBy('mostRecent')}} >Most Recent</button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={processedArticle}/>
         </div>
     );
 
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
-function Articles() {
+function Articles({articles}) {
+
+    console.log('received')
+    console.log(articles)
+    
+    const renderArticles = (articles) => {
+        return articles.map(article => {
+            return(
+                <tr data-testid="article" key="article-index">
+                    <td data-testid="article-title">{article.title}</td>
+                    <td data-testid="article-upvotes">{article.upvotes}</td>
+                    <td data-testid="article-date">{article.date}</td>
+                </tr>
+            )
+        })
+    }
 
     return (
         <div className="card w-50 mx-auto">
@@ -13,11 +28,7 @@ function Articles() {
                 </tr>
                 </thead>
                 <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
+                    {renderArticles(articles)}
                 </tbody>
             </table>
         </div>
